f1acb9440e8693589439aabc6484027374189a63
Kenzeio <115766913+Kenzeio@users.noreply.github.com>
2024-12-05 21:24:04 +0800

    bro

--- resources/js/Pages/CSHCommittee/Dashboard.vue
+++ resources/js/Pages/CSHCommittee/Dashboard.vue
@@ -116,11 +116,8 @@
                                             <p class="text-sm text-blue-500 mb-3">{{ assignedCollege.assigned_team.college.name }}</p>
                                         </div>
                                         <div class="bg-blue-50 h-full rounded-lg p-6">
-<<<<<<< HEAD
-                                            <svg class="w-8 h-8 mb-6 text-blue-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill="currentColor" d="M15 14s1 0 1-1s-1-4-5-4s-5 3-5 4s1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276c.593.69.758 1.457.76 1.72l-.008.002l-.014.002zM11 7a2 2 0 1 0 0-4a2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0a3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904c.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z"/></svg>                                            
-=======
                                             <i class="fa-solid fa-graduation-cap text-blue-700 w-8 h-8 mb-6 text-3xl"></i>
->>>>>>> 9242572d25f6bb6911518591c2216c041d0c3a73
+
                                             <p class="text-sm text-blue-600 mb-1.5">Total number of Students</p>
                                             <p class="text-2xl font-bold text-blue-700">{{ students.length }}</p>
                                         </div>
